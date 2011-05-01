b3b7a22cb4dfdb1b97794cd417f8249f7913db04
Jim Kukunas <james.t.kukunas@linux.intel.com>
2011-05-01 10:21:24 +0300

    Panel header

---
+++ panelview.h
@@ -0,0 +1,57 @@
+#ifndef _PANELVIEW_H
+#define _PANELVIEW_H 
+
+#include<QString>
+#include<QSize>
+#include<QDeclarativeView>
+#include<QPaintEvent>
+#include<QKeyEvent>
+#include<QMouseEvent>
+#include<QTabletEvent>
+#include<QDeclarativeImageProvider>
+
+#include"dialog.h"
+
+
+class PMonitor;
+class PanelView;
+
+class PMonitor : public Dialog {
+	Q_OBJECT
+public:
+	PMonitor(void);
+	friend class PanelView;
+};
+
+
+class PanelView : public Dialog,
+		  public QDeclarativeImageProvidier {
+
+	Q_OBJECT
+public:
+	PanelView(void);
+	~PanelView(void);
+
+	void paintEvent(QPaintEvent *);
+	void keyPressEvent(QKeyEvent *);
+	void keyReleaseEvent(QKeyEvent *);
+	void mouseDoubleClickEvent(QMouseEvent *);
+	void mousePressEvent(QMouseEvent *);
+	void mouseReleaseEvent(QMouseEvent *);
+	void tabletEvent(QTabletEvent *);
+
+	void regenerate(void);
+	
+	QPixmap requestPixmap(const QString&, QSize *, const QSize&);
+	
+public slots:
+	void invalidate(void);
+private:
+	bool dirty;
+	QPixmap *cache;
+	QPainter *p;
+	PMonitor *real;
+};
+
+#endif 
+
